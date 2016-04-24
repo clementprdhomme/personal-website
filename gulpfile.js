c95ab2016973594795c7922e049153464532c278
Clément Prod'homme <contact@clementprodhomme.fr>
2016-04-24 10:47:30 +0200

    Configures the development environment

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var postcssHexRbga = require('postcss-hexrgba');
+var postcssImport = require('postcss-import');
+var postcssMediaMinmax = require('postcss-media-minmax');
+var postcssNested = require('postcss-nested');
+var postcssSimpleVars = require('postcss-simple-vars');
+var cssmin = require('gulp-cssmin');
+
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    }
+  });
+});
+
+gulp.task('css', function () {
+  var processors = [
+    autoprefixer({ browsers: '> 5%' }),
+    postcssHexRbga,
+    postcssImport,
+    postcssMediaMinmax,
+    postcssNested,
+    postcssSimpleVars
+  ];
+
+    return gulp.src('src/styles/**/*.css')
+        .pipe(postcss(processors))
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/'))
+        .pipe(reload({ stream: true }));
+});
+
+gulp.task('html', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/styles/**/*.css', [ 'css' ]);
+    gulp.watch('src/**/*.html', [ 'html' ]);
+});
+
+gulp.task('default', [ 'serve', 'css', 'html', 'watch' ]);
